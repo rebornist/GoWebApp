f10ff092e219740b994d328b199a0203220ad8d1
unknown <rebornist@naver.com>
2020-01-14 07:40:58 +0900

    main.go 모든 웹 요청을 라우터가 처리하도록 수정

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
 func main() {
-	// "/" 경로로 접속했을 때 처리할 핸들러 함수 지정
-	// http.HandleFunc 함수의 첫 번째 매개 변수로 전달하는 url은 정적 문자열만 사용할 수 있다.
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// "welcome" 문자열을 화면에 출력
-		fmt.Fprintln(w, "welcome!")
-	})
+	/*
+	   // "/" 경로로 접속했을 때 처리할 핸들러 함수 지정
+	   // http.HandleFunc 함수의 첫 번째 매개 변수로 전달하는 url은 정적 문자열만 사용할 수 있다.
+	   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	   	// "welcome" 문자열을 화면에 출력
+	   	fmt.Fprintln(w, "welcome!")
+	   })
 
-	// "/about" 경로로 접속했을 때 처리할 핸들러 함수 지정
-	// http.HandleFunc 함수의 첫 번째 매개 변수로 전달하는 url은 정적 문자열만 사용할 수 있다.
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		// "about" 문자열을 화면에 출력
-		fmt.Fprintln(w, "about")
-	})
+	   // "/about" 경로로 접속했을 때 처리할 핸들러 함수 지정
+	   // http.HandleFunc 함수의 첫 번째 매개 변수로 전달하는 url은 정적 문자열만 사용할 수 있다.
+	   http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+	   	// "about" 문자열을 화면에 출력
+	   	fmt.Fprintln(w, "about")
+	   })
 
-	// 8080 포트로 웹 서버 구동
-	http.ListenAndServe(":8080", nil)
+	   // 8080 포트로 웹 서버 구동
+	   http.ListenAndServe(":8080", nil)
+	*/
 }
